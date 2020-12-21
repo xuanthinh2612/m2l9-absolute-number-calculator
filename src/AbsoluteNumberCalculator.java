80bd9f8a68fa69ba151152ac753caf5a56fb70e0
gio lang du <you@example.com>
2020-12-21 11:26:59 +0700

    absolute number calculator

---
+++ src/AbsoluteNumberCalculator.java
@@ -0,0 +1,10 @@
+public class AbsoluteNumberCalculator {
+
+    public static int findAbsolute(int number) {
+        if (number >= 0) {
+            return number;
+        } else {
+            return -number;
+        }
+    }
+}
